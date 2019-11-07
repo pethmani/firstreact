8e0d1d5a8439a5c793ae5f431aa30cf3df8f6a2b
pethmani <gmanimaran@gmail.com>
2019-11-07 16:15:17 -0500

    first project builed and added build folder here.

---
+++ precache-manifest.854024dcb872be4f2b97a05cd051fc77.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c720332540d41fa7c1afa162aa34ed7c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "91c09242899284772ff8",
+    "url": "/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "6ed8329e8df99b531f28",
+    "url": "/static/css/main.f05016c4.chunk.css"
+  },
+  {
+    "revision": "91c09242899284772ff8",
+    "url": "/static/js/2.162743ee.chunk.js"
+  },
+  {
+    "revision": "6ed8329e8df99b531f28",
+    "url": "/static/js/main.730c1e93.chunk.js"
+  },
+  {
+    "revision": "ae850a27f9d49cf50503",
+    "url": "/static/js/runtime-main.cff59e7d.js"
+  }
+]);
